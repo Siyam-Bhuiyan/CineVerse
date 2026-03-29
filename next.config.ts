fcaf9e64c0d9a54090cbeb7abb4ba42ed9e86fc1
Siyam-Bhuiyan <111516393+Siyam-Bhuiyan@users.noreply.github.com>
2026-03-29 16:00:54 +0600

    initialization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.fanart.tv",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
